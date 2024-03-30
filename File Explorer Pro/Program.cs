bb8f0af64d1e7d8ef2abf3c0f56a01793c4a5c10
Alex Gardaphadze <aleksandrgardaphadze@gmail.com>
2024-03-30 23:15:09 +0400

    #

--- File Explorer Pro/Program.cs
+++ File Explorer Pro/Program.cs
@@ -653,7 +653,7 @@ class FileExplorerPro
 
             MIT License
 
-        Copyright (c) 07.03.2024 Alex Gardaphadze
+        Copyright (c) 07.03.2024 ZenQuant (Alex Gardaphadze)
 
         Permission is hereby granted, free of charge, to any person obtaining a copy
         of this software and associated documentation files (the ""Software""), to deal
